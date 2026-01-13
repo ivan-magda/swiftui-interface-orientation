e46e0f3919457bcdec07577cdb2d25c514854c3c
Ivan Magda <imagda15@gmail.com>
2026-01-13 19:42:24 +0300

    Add swift-docc-plugin dependency
    Declare swift-docc-plugin as a dependency in Package.swift and add
    Package.resolved with pinned versions

--- Package.swift
+++ Package.swift
@@ -3,24 +3,27 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftUIInterfaceOrientation",
-    platforms: [
-        .iOS(.v14)
-    ],
-    products: [
-        .library(
-            name: "SwiftUIInterfaceOrientation",
-            targets: ["SwiftUIInterfaceOrientation"]
-        )
-    ],
-    targets: [
-        .target(
-            name: "SwiftUIInterfaceOrientation"
-        ),
-        .testTarget(
-            name: "SwiftUIInterfaceOrientationTests",
-            dependencies: ["SwiftUIInterfaceOrientation"]
-        )
-    ],
-    swiftLanguageModes: [.v5]
+  name: "SwiftUIInterfaceOrientation",
+  platforms: [
+    .iOS(.v14)
+  ],
+  products: [
+    .library(
+      name: "SwiftUIInterfaceOrientation",
+      targets: ["SwiftUIInterfaceOrientation"]
+    )
+  ],
+  dependencies: [
+    .package(url: "https://github.com/swiftlang/swift-docc-plugin", from: "1.1.0")
+  ],
+  targets: [
+    .target(
+      name: "SwiftUIInterfaceOrientation"
+    ),
+    .testTarget(
+      name: "SwiftUIInterfaceOrientationTests",
+      dependencies: ["SwiftUIInterfaceOrientation"]
+    ),
+  ],
+  swiftLanguageModes: [.v5]
 )
