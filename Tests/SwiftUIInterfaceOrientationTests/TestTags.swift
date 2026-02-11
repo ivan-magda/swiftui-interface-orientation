43ac94b8174ce04d6391e462802e2fa6cf58f318
Ivan Magda <imagda15@gmail.com>
2026-02-11 15:36:32 +0300

    Add test infrastructure with tags and helper utilities

---
+++ Tests/SwiftUIInterfaceOrientationTests/TestTags.swift
@@ -0,0 +1,8 @@
+import Testing
+
+extension Tag {
+    @Tag static var registration: Self
+    @Tag static var intersection: Self
+    @Tag static var configuration: Self
+    @Tag static var infoPlist: Self
+}
